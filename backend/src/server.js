2fbd5bd5e76640630534d7530c0ba287ed931fcc
Faizal Amin <faizalamin0000@gmail.com>
2025-11-17 16:41:32 +0530

    Fix axios baseURL for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,13 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" })); // req.body
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL,  // your Vercel site
+  ],
+  credentials: true,
+}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
