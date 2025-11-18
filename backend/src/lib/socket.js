3ce3ef6767b56cff33da05f55ba97faed9ce57ee
Faizal Amin <faizalamin0000@gmail.com>
2025-11-18 13:31:27 +0530

    seconds

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://chatlify-80ig198le-faizals-projects-96c8bb70.vercel.app"
+     "https://chatlify.vercel.app",  
     ],
     credentials: true,
   },
